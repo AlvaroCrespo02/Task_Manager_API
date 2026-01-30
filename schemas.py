cc74af9f1da12b782ddda27ac42ef21773174884
AlvaroCrespo02 <118764704+AlvaroCrespo02@users.noreply.github.com>
2026-01-30 15:53:07 +0100

    Added error validation :)

--- schemas.py
+++ schemas.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -22,6 +25,8 @@ tasks: list[dict] = [
     },
 ]
 
+# ENDPOINTS
+
 @app.get("/", include_in_schema=False)
 def root(request: Request):
     return templates.TemplateResponse(request, 
@@ -29,19 +34,83 @@ def root(request: Request):
                                       {"title": "Home"})
 
 @app.get("/tests")
-def taskList(request: Request):
+def task_page(request: Request):
     return templates.TemplateResponse(request, 
                                       "tests.html", 
                                       {"tasks": tasks, "title": "Tests"})
 
 @app.get("/tests/{task_id}")
-def get_task(request: Request, task_id: int):
+def list_tasks(request: Request, task_id: int):
     for task in tasks:
         if task.get("id") == task_id:
             return templates.TemplateResponse(request, 
                                       "detailedTask.html", 
-                                      {"task": task, "title": "Details"})
+                                      {"task": task, "title": task["task"]}
+                                      )
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, 
         detail="Post was not found"
         )
+
+@app.get("/api/tests")
+def get_tasks():
+    return tasks
+
+@app.get("/api/tests/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.get("id") == task_id:
+            return task
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+# This deals with the Starlette exceptions
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    # Build the message if it has specific details
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occurred. Please check your equest and try again."
+    )
+    # If the url starts with /api we return a JSON response with the exception
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"Details": message}
+        )
+    # If it's not an API url we return the template with the exception
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code 
+        # This makes sure we get the correct HTTP status code
+        # otherwise we would get a 200 response
+    )
+
+# Validation error handler
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    # If the url starts with /api we return a JSON response with the exception
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"Details": exception.errors()}
+        )
+    # If it's not an API url we return the template with the exception
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again",
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT
+        # This makes sure we get the correct HTTP status code
+        # otherwise we would get a 200 response
+    )
