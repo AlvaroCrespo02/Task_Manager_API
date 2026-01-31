a2dbf78ac1873be873e334823987d418becdd1a8
AlvaroCrespo02 <118764704+AlvaroCrespo02@users.noreply.github.com>
2026-01-31 12:32:13 +0100

    Added db actions on client side (list/delete) :)

--- main.py
+++ main.py
@@ -13,24 +13,6 @@ app = FastAPI()
 Base.metadata.create_all(engine)
 templates = Jinja2Templates(directory="templates")
 
-# Mock data
-tasks: list[dict] = [
-    {
-        "id": 1, 
-        "task":'Buy groceries', 
-        "created":'2026-02-26', 
-        "due":'2026-03-26', 
-        "done":True
-    },
-    {
-        "id": 2, 
-        "task":'Cook lunch', 
-        "created":'2026-01-26', 
-        "due":'2026-01-26', 
-        "done":False
-    },
-]
-
 # ENDPOINTS
 
 @app.get("/", include_in_schema=False)
@@ -39,36 +21,50 @@ def root(request: Request):
                                       "index.html", 
                                       {"title": "Home"})
 
-@app.get("/tasks")
-def task_page(request: Request):
+@app.get("/tasks", include_in_schema=False)
+def task_page(request: Request, db: Session= Depends(get_db)):
+    task_list = db.query(Task).all()
     return templates.TemplateResponse(request, 
                                       "tasks.html", 
-                                      {"tasks": tasks, "title": "Tasks"})
+                                      {"tasks": task_list, "title": "Tasks"})
 
-@app.get("/tasks/{task_id}")
-def list_tasks(request: Request, task_id: int):
-    for task in tasks:
-        if task.get("id") == task_id:
+@app.get("/tasks/{task_id}", include_in_schema=False)
+def list_tasks(request: Request, task_id: int, db: Session= Depends(get_db)):
+    task_list = db.query(Task).all()
+    for task in task_list:
+        if task.id == task_id:
             return templates.TemplateResponse(request, 
                                       "detailedTask.html", 
-                                      {"task": task, "title": task["task"]}
+                                      {"task": task, "title": task.task}
                                       )
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, 
         detail="Post was not found"
         )
 
+@app.get("/tasks/{task_id}", status_code=status.HTTP_200_OK, include_in_schema=False)
+def delete_task(request: Request, task_id: int, db: Session = Depends(get_db)):
+    task = db.query(Task).get(task_id)
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+    db.delete(task)
+    db.commit()
+    return templates.TemplateResponse(request,
+                                      "deletedTask.html",
+                                      {"task": task, "title": task.task}
+                                      )
+
 @app.get("/api/dbhealth")
-def check_db(db: Session = Depends(get_db)):
+def api_check_db(db: Session = Depends(get_db)):
     return {"status": "db connection OK"}
 
 @app.get("/api/tasks", response_model=list[TaskResponse])
-def listTasks(db: Session = Depends(get_db)):
+def api_list_tasks(db: Session = Depends(get_db)):
     taskList = db.query(Task).all()
     return taskList
 
 @app.post("/api/tasks", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
-def create_task(task: TaskCreate, db: Session= Depends(get_db)):
+def api_create_task(task: TaskCreate, db: Session= Depends(get_db)):
     new_task = Task(
         task = task.task,
         created = date.today(),
@@ -79,9 +75,8 @@ def create_task(task: TaskCreate, db: Session= Depends(get_db)):
     db.commit()
     return new_task
 
-@app.get("/api/tasks/{task_id}/delete", status_code=status.HTTP_200_OK)
-def deleteTask(task_id: int, db: Session = Depends(get_db)):
-    print("Task ID: ", task_id)
+@app.delete("/api/tasks/{task_id}", status_code=status.HTTP_200_OK)
+def api_delete_task(task_id: int, db: Session = Depends(get_db)):
     myTask = db.query(Task).get(task_id)
     if myTask is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
@@ -91,9 +86,10 @@ def deleteTask(task_id: int, db: Session = Depends(get_db)):
     
 
 @app.get("/api/tasks/{task_id}", response_model=TaskResponse)
-def get_task(task_id: int):
-    for task in tasks:
-        if task.get("id") == task_id:
+def api_get_task(task_id: int, db: Session = Depends(get_db)):
+    task_list = db.query(Task).all()
+    for task in task_list:
+        if task.id == task_id:
             return task
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
 
