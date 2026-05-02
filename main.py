bfbf415ba5e36085f05c22b7b776d708bda07496
AlvaroCrespo02 <118764704+AlvaroCrespo02@users.noreply.github.com>
2026-05-02 12:45:17 +0200

    First frontend commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.templating import Jinja2Templates
 
 from contextlib import asynccontextmanager
 from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
@@ -25,7 +26,11 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(users.router, prefix="/api/users", tags=["users"])
 app.include_router(tasks.router, prefix="/api/tasks", tags=["tasks"])
 
+# ============================================================
+# Jinja2 Templates
+# ============================================================
 
+templates = Jinja2Templates(directory="templates")
 
 # ============================================================
 # Exception Handlers
@@ -34,11 +39,38 @@ app.include_router(tasks.router, prefix="/api/tasks", tags=["tasks"])
 # This deals with the Starlette exceptions
 @app.exception_handler(StarletteHTTPException)
 async def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error occurred. Please check your request and try again."
+    )
+
     if request.url.path.startswith("/api"):
         return await http_exception_handler(request, exception)
+    
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": exception.status_code,
+            "title": exception.status_code,
+            "message": message,
+        },
+        status_code=exception.status_code
+    )
 
 # Validation error handler
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exception: RequestValidationError):
     if request.url.path.startswith("/api"):
         return await request_validation_exception_handler(request, exception)
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        {
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "message": "Invalid request. Please check your input and try again.",
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT
+    )
