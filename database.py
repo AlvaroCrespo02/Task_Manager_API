5450abcffd17c367f1b1e1aa946c620da9d13190
AlvaroCrespo02 <118764704+AlvaroCrespo02@users.noreply.github.com>
2026-06-04 18:50:46 +0200

    Modified database.py to work with neon

--- database.py
+++ database.py
@@ -24,7 +24,7 @@ else:
 # Create database connection and session
 # engine = create_engine(db_url, echo=True)
 # engine = create_engine(db_url)
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL,connect_args={"ssl": "require"})
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
